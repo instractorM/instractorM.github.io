8a5024b96de5002c769230ef730ffadf2c31b730
InstractorM@TAVTTS <99104890+instractorM@users.noreply.github.com>
2022-02-08 14:53:02 +0900

    20220208_2nen

--- js/load.js
+++ js/load.js
@@ -2,5 +2,8 @@ $(function() {
     $("header").load("page/header.html");
     $("nav").load("page/nav.html");
     $("#top").load("page/top.html");
+    $("#app").load("page/app.html");
+    $("#text").load("page/text.html");
+    $("#link").load("page/link.html");
     $("footer").load("page/footer.html");
 });
